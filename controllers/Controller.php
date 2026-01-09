37ec9084012c4a5fe7837d44b5a5c0ee8238e11e
mariaandoli <mariaandoli@campus.monlau.com>
2026-01-09 17:52:43 +0100

    controller

---
+++ controllers/Controller.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Controllers;
+
+abstract class Controller {
+    
+    // Renderizar vista
+    protected function render($view, $data = []) {
+        extract($data);
+        
+        $viewPath = __DIR__ . '/../views/' . $view;
+        
+      
+        
+        require_once __DIR__ . '/../views/layout/header.php';
+        
+        require $viewPath;
+        
+        require_once __DIR__ . '/../views/layout/footer.php';
+    }
+    //esto es lo que hace que lo reedirige en plan segun la variable del url donde este
+    protected function redirect($url) {
+        header("Location: " . $url);
+        exit;
+    }
+}
