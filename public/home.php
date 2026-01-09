647ae3d959646ab633f7a7f99baed79dd8efcf61
mariaandoli <mariaandoli@campus.monlau.com>
2026-01-09 16:51:19 +0100

    reestructure

---
+++ public/home.php
@@ -0,0 +1,24 @@
+<?php
+require_once '../views/layout/header.php';
+require_once '../views/layout/nav.php';
+require_once '../views/layout/footer.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Virtual Closet</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+    
+    <main>
+        <p>This is the main content of the home page.</p>
+        <button onclick="location.href='login.php'">Go to Login</button>
+        <button onclick="location.href='register.php'">Go to Register</button>
+        
+    </main>
+    
+</body>
+</html>
